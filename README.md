f812ddfc7bd4ed820d976c71b90e9b6c4b7ddfcd
Yang Chiu <yang.chiu@suse.com>
2022-09-02 14:10:57 +0800

    Add action implementation
    Signed-off-by: Yang Chiu <yang.chiu@suse.com>

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-test
+# Get milestone javascript action
+
+Get milestone object from milestone name
+
+## Inputs
+
+## `token`
+
+**Required** Github token.
+
+## `repository`
+
+**Required** Github repository to be retrieved.
+
+## `name`
+
+Name of the milestone to be retrieved.
+
+## Outputs
+
+## `milestone`
+
+The retrieved milestone object.
+
+## Example usage
+
+uses: yangchiu/milestone-action@v1.0.0
+with:
+  token: ${{ github.token }}
+  repository: ${{github.repository }}
---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+  const repository = core.getInput('repo');
+  console.log(`Get Repository: ${repository}`);
+
+  const milestoneName = core.getInput('name');
+  console.log(`Get Milestone Name: ${milestoneName}`);
+
+  const token = core.getInput('token');
+  const octokit = github.getOctokit(token);
+
+  const response = await octokit.request(`GET /repos/${repository}/milestones`);
+  const data = response.data;
+
+  console.log(data);
+  core.setOutput('data', data);
+
+} catch (error) {
+
+  core.setFailed(error.message);
+  
+}
